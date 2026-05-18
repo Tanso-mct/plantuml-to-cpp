8fe608202a77be231ba08d7e484c9073309992be
Tanso <tansomctexp@gmail.com>
2026-05-18 11:57:55 +0900

    Configuration of the CMake build configuration is complete.

---
+++ plantuml-to-cpp/app/main.cpp
@@ -0,0 +1,9 @@
+﻿#include "plantuml-to-cpp/plantuml-to-cpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ plantuml-to-cpp/src/plantuml-to-cpp.cpp
@@ -0,0 +1,4 @@
+﻿// plantuml-to-cpp.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "plantuml-to-cpp/plantuml-to-cpp.h"
